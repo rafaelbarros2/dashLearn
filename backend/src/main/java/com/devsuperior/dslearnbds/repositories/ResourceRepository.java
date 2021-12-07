e4e3a514c1df1bb29d22bf7e42a459db4411dbfd
lfelipe <rafael.eng.mecanic@gmail.com>
2021-12-07 10:00:21 -0300

    repositories

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/repositories/ResourceRepository.java
@@ -0,0 +1,7 @@
+package com.devsuperior.dslearnbds.repositories;
+
+import com.devsuperior.dslearnbds.entities.Resourcer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResourceRepository extends JpaRepository<Resourcer, Long> {
+}

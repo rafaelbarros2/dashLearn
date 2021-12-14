800fbcc5463b629ae20a46e7aafc9cbc649dbee1
lfelipe <rafael.eng.mecanic@gmail.com>
2021-12-14 18:20:28 -0300

    pre authorizing method

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/resources/DeliverResource.java
@@ -0,0 +1,23 @@
+package com.devsuperior.dslearnbds.resources;
+
+import com.devsuperior.dslearnbds.dtos.DeliverRevisionDTO;
+import com.devsuperior.dslearnbds.services.DeliverService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/deliveries")
+public class DeliverResource {
+
+    @Autowired
+    private DeliverService deliverService;
+
+    @PutMapping(value = "/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN','INSTRUCTOR')")
+    public ResponseEntity<Void> saveRevision(@PathVariable Long id, @RequestBody DeliverRevisionDTO dto ){
+        deliverService.saveRevision(id, dto);
+        return ResponseEntity.noContent().build();
+    }
+}
